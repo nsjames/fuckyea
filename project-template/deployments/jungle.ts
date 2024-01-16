6bdbdc3ee5fd1b97d1a07f9cceb49c16a490c419
nsjames <DENIED>
2024-01-16 15:41:41 +0000

    add docs for deployment tester

--- project-template/deployments/jungle.ts
+++ project-template/deployments/jungle.ts
@@ -1,6 +1,13 @@
 module.exports = async (tester) => {
 
-    // the returned value will be a wharfkit contract object
+    // the returned value will be:
+    /*
+    {
+        accounts: [same as your config accounts],
+        sessions: {[accountName]: wharfkit session object},
+        deploy => (accountName, contractPath): wharfkit contract instance,
+    }
+     */
     const contract = await tester.deploy('youraccount', 'build/contract').catch(err => {
         console.error(err)
         process.exit(1);
